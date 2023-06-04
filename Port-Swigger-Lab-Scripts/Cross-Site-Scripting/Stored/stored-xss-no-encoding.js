4c589afa9983e917b855d7fb817ddd3671d7ee1f
Rogue Blue <rogueblueprint@protonmail.com>
2023-06-04 14:30:36 -0700

    Added lab links and another solution for dom based xss

--- Port-Swigger-Lab-Scripts/Cross-Site-Scripting/Stored/stored-xss-no-encoding.js
+++ Port-Swigger-Lab-Scripts/Cross-Site-Scripting/Stored/stored-xss-no-encoding.js
@@ -1,1 +1,4 @@
+// The following XSS payload is meant to solve the following lab:
+// https://portswigger.net/web-security/cross-site-scripting/stored/lab-html-context-nothing-encoded
+
 <script>alert(1)</script>
